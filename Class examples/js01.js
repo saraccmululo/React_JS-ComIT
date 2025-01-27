71cfcd0aaadd75a0698cd1da0cb9dbf28523e2ce
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-01-27 16:03:03 -0600

    created exercises in class file

---
+++ Class examples/js01.js
@@ -0,0 +1,9 @@
+const groceryItemFromInput=document.querySelector('input');
+const addButton=document.querySelector('button');
+
+function addNewGroceryItem(){
+    const itemToShow=groceryItemFromInput.value;
+    
+}
+addButton.addEventListener('click, showGroceryItem');
+showGroceryItem();
